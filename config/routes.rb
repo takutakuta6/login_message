3f83c85f4c0af7dafdaa5ac1ba434001e43e425c
拓美 <takumi.rug@gmail.com>
2020-07-17 09:28:47 +0900

    管理者画面の設定完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :messages
   devise_for :users
   root to: "top#index"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-user1 = User.create!(email: "test@example.com", password: "password")
-user2 = User.create!(email: "hoge@example.com", password: "password")
+# user1 = User.create!(email: "test@example.com", password: "password")
+# user2 = User.create!(email: "hoge@example.com", password: "password")
 
-user1.messages.create!(content: "おはよう")
-user2.messages.create!(content: "こんにちは")
-user1.messages.create!(content: "こんばんは")
-user1.messages.create!(content: "おやすみ")
+# user1.messages.create!(content: "おはよう")
+# user2.messages.create!(content: "こんにちは")
+# user1.messages.create!(content: "こんばんは")
+# user1.messages.create!(content: "おやすみ")
+AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password") if Rails.env.development?
